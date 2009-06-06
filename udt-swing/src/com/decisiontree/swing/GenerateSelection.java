7e8c279395100026e54db17a3c42502cdb882721
smithtsang <smithtsang@3e8f520a-523f-11de-8d25-355ef5ecef59>
2009-06-06 14:30:11 +0000

    Remove special characters

--- udt-swing/src/com/decisiontree/swing/GenerateSelection.java
+++ udt-swing/src/com/decisiontree/swing/GenerateSelection.java
@@ -1,26 +1,35 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * GenerateSelection.java
+/**
+ * Decision Tree Classification With Uncertain Data (UDT)
+ * Copyright (C) 2009, The Database Group,
+ * Department of Computer Science, The University of Hong Kong
  *
- * Created on 2009/6/6, 下午 07:39:09
+ * This file is part of UDT.
+ *
+ * UDT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * UDT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.decisiontree.swing;
 
 import com.decisiontree.datagen.DataGen;
-import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import com.decisiontree.param.GlobalParam;
 
 /**
- *
- * @author Smith
+ * GenerateSelection
+ * @author Smith Tsang
+ * @since 0.9
  */
 public class GenerateSelection extends javax.swing.JPanel {
 
